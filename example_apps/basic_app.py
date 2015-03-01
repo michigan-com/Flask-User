972dbcb1de6f181cb2633406718f3a125b81607e
Eric Bower <neurosnap@gmail.com>
2015-03-01 10:46:20 -0500

    fixed bytestring error in python3 with secret key

--- example_apps/basic_app.py
+++ example_apps/basic_app.py
@@ -27,7 +27,7 @@ class ConfigClass(object):
 
 def create_app():
     """ Flask application factory """
-    
+
     # Setup Flask app and app.config
     app = Flask(__name__)
     app.config.from_object(__name__+'.ConfigClass')
@@ -94,5 +94,5 @@ def create_app():
 # Start development web server
 if __name__=='__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
